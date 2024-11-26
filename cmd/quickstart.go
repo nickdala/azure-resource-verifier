51f0bcfb3e0ba293f6fefb21ec6022b4ec536d44
Nick Dalalelis <ndalalelis@gmail.com>
2024-11-25 20:12:47 -0500

    quickstart command and remove wizard

--- cmd/quickstart.go
+++ cmd/quickstart.go
@@ -7,7 +7,11 @@ import (
 	"os"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/nickdala/azure-resource-verifier/cmd/modal/appservice"
+	"github.com/nickdala/azure-resource-verifier/cmd/modal/database"
+	"github.com/nickdala/azure-resource-verifier/internal/azure"
 	"github.com/nickdala/azure-resource-verifier/internal/cli"
+	"github.com/nickdala/azure-resource-verifier/internal/table"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -15,9 +19,8 @@ import (
 // quickstartCmd represents the quickstart command
 var quickstartCmd = &cobra.Command{
 	Use:   "quickstart",
-	Short: "Quickstart azure-resource-verifier",
-	Long: `The quickstart command provide the means to quickly get started with azure-resource-verifier.
-It will guide you through the process of creating a new configuration file and running a verification.`,
+	Short: "Quickstart Azure Resource Verifier",
+	Long:  `The quickstart command command provides a guided experience that shows the regions the Azure resources can be deployed.`,
 
 	RunE: cli.AzureClientWrapRunE(quickStartCommand),
 }
@@ -28,7 +31,110 @@ func quickStartCommand(cmd *cobra.Command, _ []string, cred *azidentity.DefaultA
 	subscriptionId := viper.GetString("subscription-id")
 	log.Printf("subscription-id: %s", subscriptionId)
 
+	azureLocations, err := getLocations(cmd, cred, ctx, subscriptionId)
+	if err != nil {
+		return cli.CreateAzrErr("Error parsing location flag", err)
+	}
+
+	databases, _ := database.ShowDatabaseModalAndGetChoices()
+	appService, _ := appservice.ShowAppServiceModalAndGetChoices()
+
+	switch appService {
+	case appservice.APP_SERVICE_LINUX_CODE:
+		println("Selected: Azure App Service - Linux Code")
+		azureLocations, err = getLocationsForAppService(azureLocations, cred, ctx, subscriptionId, azure.Linux, azure.Code)
+		if err != nil {
+			return cli.CreateAzrErr("Error getting App Service locations", err)
+		}
+	case appservice.APP_SERVICE_LINUX_CONTAINER:
+		println("Selected: Azure App Service - Linux Container")
+		azureLocations, err = getLocationsForAppService(azureLocations, cred, ctx, subscriptionId, azure.Linux, azure.Container)
+		if err != nil {
+			return cli.CreateAzrErr("Error getting App Service locations", err)
+		}
+	case appservice.APP_SERVICE_WINDOWS_CODE:
+		println("Selected: Azure App Service - Windows Code")
+		azureLocations, err = getLocationsForAppService(azureLocations, cred, ctx, subscriptionId, azure.Windows, azure.Code)
+		if err != nil {
+			return cli.CreateAzrErr("Error getting App Service locations", err)
+		}
+	case appservice.APP_SERVICE_WINDOWS_CONTAINER:
+		println("Selected: Azure App Service - Windows Container")
+		azureLocations, err = getLocationsForAppService(azureLocations, cred, ctx, subscriptionId, azure.Windows, azure.Container)
+		if err != nil {
+			return cli.CreateAzrErr("Error getting App Service locations", err)
+		}
+	}
+
+	for _, db := range databases {
+		switch db {
+		case database.REDIS:
+			println("Selected: Azure Cache for Redis")
+			azureLocations, err = getLocationsForRedis(azureLocations, cred, ctx, subscriptionId)
+			if err != nil {
+				return cli.CreateAzrErr("Error getting Redis locations", err)
+			}
+		case database.POSTGRESQL:
+			println("Selected: Azure PostgreSQL Flexible Server")
+			azureLocations, err = getPostgresLocations(subscriptionId, cred, ctx, azureLocations, false)
+			if err != nil {
+				return cli.CreateAzrErr("Error getting PostgreSQL locations", err)
+			}
+		case database.POSTGRESQL_HA:
+			println("Selected: Azure PostgreSQL Flexible Server with HA")
+			azureLocations, err = getPostgresLocations(subscriptionId, cred, ctx, azureLocations, true)
+			if err != nil {
+				return cli.CreateAzrErr("Error getting PostgreSQL HA locations", err)
+			}
+		}
+	}
+
+	var data [][]string
+	for _, location := range azureLocations.Value {
+		data = append(data, []string{location.Name, location.DisplayName})
+	}
+
+	table := table.NewTable(table.Locations)
+	table.AppendBulk(data)
+	table.Render()
+
 	return nil
+}
+
+func getLocationsForAppService(locations *azure.AzureLocationList, cred *azidentity.DefaultAzureCredential, ctx context.Context, subscriptionId string, os azure.AppServiceOS, publishType azure.AppServicePublishType) (*azure.AzureLocationList, error) {
+	azureAppService := azure.NewAzureAppService(cred, ctx, subscriptionId)
+	appServiceLocations, err := azureAppService.GetAppServiceLocations(locations, os, publishType)
+	if err != nil {
+		return nil, fmt.Errorf("error getting App Service locations %w", err)
+	}
+
+	return locations.Intersection(appServiceLocations), nil
+}
+
+func getLocationsForRedis(locations *azure.AzureLocationList, cred *azidentity.DefaultAzureCredential, ctx context.Context, subscriptionId string) (*azure.AzureLocationList, error) {
+	redisCache := azure.NewAzureRedisCache(cred, ctx, subscriptionId)
+	redisLocations, err := redisCache.GetRedisLocations()
+	if err != nil {
+		return nil, fmt.Errorf("error getting Redis locations %w", err)
+	}
+
+	return locations.Intersection(redisLocations), nil
+}
+
+func getPostgresLocations(subscriptionId string, cred *azidentity.DefaultAzureCredential, ctx context.Context, locations *azure.AzureLocationList, haEnabled bool) (*azure.AzureLocationList, error) {
+
+	azurePostgresql := azure.NewAzurePostgresqlFlexibleServer(cred, ctx, subscriptionId)
+
+	postgresLocations, postgresqlHaLocations, _, err := azurePostgresql.GetPostgresqlLocations(locations)
+	if err != nil {
+		return nil, fmt.Errorf("error getting PostgreSQL locations %w", err)
+	}
+
+	if haEnabled {
+		return locations.Intersection((*azure.AzureLocationList)(postgresqlHaLocations)), nil
+	} else {
+		return locations.Intersection((*azure.AzureLocationList)(postgresLocations)), nil
+	}
 }
 
 func init() {
